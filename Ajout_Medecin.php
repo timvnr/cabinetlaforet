d11bc82dc46c94fe6eb7bfeab91a0533caa6c2ac
timvrn <97245541+timvrn@users.noreply.github.com>
2022-01-06 22:19:17 +0100

    test

--- Ajout_Medecin.php
+++ Ajout_Medecin.php
@@ -26,18 +26,14 @@ require('verif.php'); //il faut executer avant le fichier verif.php qui se conne
 			$prenom = $_POST['prenom'];
 
 			//Requête pour rechercher l'id d'un médecin (idM) à partir de son nom et de son prénom
-			$res2 = $linkpdo->prepare("SELECT idM FROM medecin WHERE nom=:nom and prenom=:prenom");
-			$res2 -> execute(array('nom' => $nom, 'prenom' => $prenom));
+			$res2 = $linkpdo->query("SELECT idM FROM medecin WHERE nom='$nom' and prenom='$prenom'");
 			$data = $res2->fetch();
-			echo $data[0];
 
 			//Si le médecin n'existe pas dans la base de données, on l'ajoute avec la requete ci-dessous
 			if($data['idM']==null) {
 
 				//Requête pour ajouter un médecin dans la base de données à partir des informations saisies par l'utilisateur
-				$res = $linkpdo->prepare("INSERT INTO medecin(idM, civilite, nom, prenom) VALUES('$id', '$civilite', '$nom', '$prenom')");
-				$res -> execute(array('id' => $id, 'civilite' => $civilite,'nom' => $nom, 'prenom' => $prenom));
-				var_dump($res);
+				$res = $linkpdo->exec("INSERT INTO medecin(idM, civilite, nom, prenom) VALUES('$id', '$civilite', '$nom', '$prenom')");
 				//Redirection vers la page d'affichage des médecins
 				header('Location: Affichage_Medecin.php');
 			}
