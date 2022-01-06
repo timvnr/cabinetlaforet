efc7b7e4a78d648f48e0f92bae722cebc3f62729
eliot312 <97245710+eliot312@users.noreply.github.com>
2022-01-06 20:26:04 +0100

    test17

--- Ajout_Usagers.php
+++ Ajout_Usagers.php
@@ -40,7 +40,8 @@ require('verif.php'); //il faut executer avant le fichier verif.php qui se conne
 			$data = $res2->fetch();
 			
 			if($data[0]==null) {
-				$res = $linkpdo->query("INSERT INTO usager(idU, civilite, nom, prenom, adresse, codeP, dateN, lieuN, numS, idM) VALUES('', '$civilite', '$nom', '$prenom', '$adresse', '$code', '$dateN', '$lieuN', '$numS', '$idM')");
+				$req = $r -> prepare('INSERT INTO usager (idU,civilite,nom,prenom,adresse,codeP, dateN,lieuN,numS,idM) VALUES ('',:civilite, :nom, :prenom, :adresse, :codeP, :dateN, :lieuN, :numS, :idM)'); 
+				$req->execute(array('civilite'=>$civilite,'nom'=>$nom,'prenom'=>$prenom,'adresse'=>$adresse,'codeP'=>$codeP, 'dateN'=>$dateN,'lieuN'=>$lieuN, 'numS'=>$numS, 'idM'=>$idM));
 			}
 
 			//redirection vers la page d'affichage des patients (usagers)
